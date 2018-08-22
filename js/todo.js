89115b2c7bccc1731be9d2c0f090f5a7d88c0e73
estacioj91 <estacioj91@yahoo.com>
2018-08-22 14:07:12 -0700

    practice with event handlers
    practice with event handlers

--- js/todo.js
+++ js/todo.js
@@ -34,3 +34,8 @@ todos.forEach(function(todo){
     p.textContent = todo.text;
     document.querySelector("body").appendChild(p);
 })
+
+
+document.querySelector("button").addEventListener("click", function(event){
+    console.log(event);
+})
