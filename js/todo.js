e602a54a3afd24c84fd051c4df501321b4412661
estacioj91 <estacioj91@yahoo.com>
2018-08-22 19:34:00 -0700

    Render filtered todos
    Render filtered todos

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,4 @@
-const todos = [
-    {
+const todos = [{
         text: "Order cat food",
         completed: false
     },
@@ -20,22 +19,39 @@ const todos = [
         completed: false
     },
 ]
+const filters = {
+    searchText: ""
+}
+const renderTodos =  function(todos, filters){
+    const filteredTodos = todos.filter(function (todo){
+        return todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
+    })
+    const incompleteTodos = filteredTodos.filter(function (todo) {
+        return !todo.completed;
+    })
+    document.querySelector("#todos").innerHTML = "";
+    const summary = document.createElement("h2");
+    summary.textContent = `You have ${incompleteTodos.length} todos left`;
+    document.querySelector("#todos").appendChild(summary);
+    
+    filteredTodos.forEach(function (todo) {
+        const p = document.createElement("p");
+        p.textContent = todo.text;
+        document.querySelector("#todos").appendChild(p);
+    })
+}
 
-const incompleteTodos = todos.filter(function(todo){
-    return !todo.completed;
+renderTodos(todos, filters);
+
+document.querySelector("#add-todo").addEventListener("click", function (event) {
+
 })
-
-const summary = document.createElement("h2");
-summary.textContent = `You have ${incompleteTodos.length} todos left`;
-document.querySelector("body").appendChild(summary);
-
-todos.forEach(function(todo){
+document.querySelector("#new-todo").addEventListener("input", function(event){
     const p = document.createElement("p");
-    p.textContent = todo.text;
+    p.textContent = event.target.value;
     document.querySelector("body").appendChild(p);
-})
-
-
-document.querySelector("button").addEventListener("click", function(event){
-    console.log(event);
+});
+document.querySelector("#searchText").addEventListener('input', function(event){
+    filters.searchText = event.target.value;
+    renderTodos(todos,filters);
 })
