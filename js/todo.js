f8fb408075356a697d0554338bf010817cd03c12
estacioj91 <estacioj91@yahoo.com>
2018-08-25 18:15:23 -0700

    Code Refactor
    Erased old comments, split code to new file

--- js/todo.js
+++ js/todo.js
@@ -1,58 +1,8 @@
-//Dummy Data
-// const todos = [{
-//         text: "Order cat food",
-//         completed: false
-//     },
-//     {
-//         text: "Clean kitchen",
-//         completed: true
-//     },
-//     {
-//         text: "Buy food",
-//         completed: true
-//     },
-//     {
-//         text: "Exercise",
-//         completed: true
-//     },
-//     {
-//         text: "Do work",
-//         completed: false
-//     },
-// ]
-
-let todos = [];
+let todos = getSavedTodos();
 const filters = {
     searchText: "",
     hideCompleted: false
 }
-const todosJSON = localStorage.getItem("todos");
-if(todosJSON !== null){
-    todos = JSON.parse(todosJSON);
-}
-
-
-const renderTodos = function (todos, filters) {
-    const filteredTodos = todos.filter(function (todo) {
-        const searchTextMatch =todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
-        const hideCompletedMatch =!filters.hideCompleted || !todo.completed;
-        return searchTextMatch && hideCompletedMatch;
-    })
-    const incompleteTodos = filteredTodos.filter(function (todo) {
-        return !todo.completed;
-    })
-    document.querySelector("#todos").innerHTML = "";
-    const summary = document.createElement("h2");
-    summary.textContent = `You have ${incompleteTodos.length} todos left`;
-    document.querySelector("#todos").appendChild(summary);
-
-    filteredTodos.forEach(function (todo) {
-        const p = document.createElement("p");
-        p.textContent = todo.text;
-        document.querySelector("#todos").appendChild(p);
-    })
-}
-
 renderTodos(todos, filters);
 
 document.querySelector("#searchText").addEventListener('input', function (event) {
@@ -67,7 +17,7 @@ document.querySelector("#new-todo").addEventListener(
             text: event.target.elements.text.value,
             completed: false
         });
-        localStorage.setItem("todos", JSON.stringify(todos));
+        saveTodos(todos);
         renderTodos(todos,filters);
         //clears form
         event.target.elements.text.value = ""
