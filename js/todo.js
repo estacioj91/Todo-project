0274138a8d08c6fc69960bacc90e0ffbc7c875fc
estacioj91 <estacioj91@yahoo.com>
2018-09-04 19:01:27 -0700

    minor change

--- js/todo.js
+++ js/todo.js
@@ -7,33 +7,33 @@ const filters = {
 }
 renderTodos(todos, filters);
 
-document.querySelector("#searchText").addEventListener('input',  (event) => {
+document.querySelector("#searchText").addEventListener('input', (event) => {
     filters.searchText = event.target.value;
     renderTodos(todos, filters);
 })
 document.querySelector("#new-todo").addEventListener(
     "submit",
-     (event) =>{
-         const text = event.target.elements.text.value.trim();
-         event.preventDefault();
-         if(text.length >0){
+    (event) => {
+        const text = event.target.elements.text.value.trim();
+        event.preventDefault();
+        if (text.length > 0) {
             todos.push({
                 id: uuidv4(),
                 text,
                 completed: false
             });
             saveTodos(todos);
-            renderTodos(todos,filters);
+            renderTodos(todos, filters);
             //clears form
             event.target.elements.text.value = ""
-         }
-        
-        
+        }
+
+
     }
 )
 document.querySelector("#hide-completed").addEventListener(
-    "change", (event)=>{
+    "change", (event) => {
         filters.hideCompleted = event.target.checked;
-        renderTodos(todos,filters);
+        renderTodos(todos, filters);
     }
 )
